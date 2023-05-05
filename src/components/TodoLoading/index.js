6a5bdd2af460cf69a7a353df3690390b23a3e79a
Fernanda Ramírez <mframirezfuentes@gmail.com>
2023-05-05 17:13:01 -0400

    Fix a Icon in loading component

--- src/components/TodoLoading/index.js
+++ src/components/TodoLoading/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './TodosLoading.css'
 import { TodoIcon } from '../TodoIcon'
+import { RxReload } from "react-icons/rx";
 
 const TodosLoading = () => {
   return (
     <div className='LoadingTodo-container'>
-      <TodoIcon/>
+      <TodoIcon IconType={RxReload} IconColor={"gray"}/>
       <p className='LoadingTodo-text'>Estamos cargango, esperanos un momento</p>
-      <TodoIcon/>
+
     </div>
 
   )
